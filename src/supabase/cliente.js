3927ecc0ca1793163d5325754870e07ad3d79d55
Viniciusezequiel <164230144+Viniciusezequiel@users.noreply.github.com>
2026-02-12 12:36:25 -0300

    Update cliente.js

--- src/supabase/cliente.js
+++ src/supabase/cliente.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Certifique-se de que as variáveis começam com VITE_ para Vite
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
