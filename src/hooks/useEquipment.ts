442fa013afec3d3503e17777dbb6eebe5464e7f1
Viniciusezequiel <164230144+Viniciusezequiel@users.noreply.github.com>
2026-02-12 15:36:15 -0300

    Update useEquipment.ts

--- src/hooks/useEquipment.ts
+++ src/hooks/useEquipment.ts
@@ -1,6 +1,6 @@
 // src/hooks/useEquipment.ts
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '@/lib/supabaseClient';
 
 // Listar equipamentos
 export const useEquipmentList = () => {
