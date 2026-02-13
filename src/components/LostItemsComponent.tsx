cfdfe2f69e308da65ac2c39f3fa225b719260122
Viniciusezequiel <164230144+Viniciusezequiel@users.noreply.github.com>
2026-02-13 13:29:53 -0300

    Create LostItemsComponent.tsx

---
+++ src/components/LostItemsComponent.tsx
@@ -0,0 +1,12 @@
+import LostItemsComponent from './components/LostItemsComponent'
+
+function App() {
+  return (
+    <div>
+      <h1>Meu Sistema</h1>
+      <LostItemsComponent />
+    </div>
+  )
+}
+
+export default App
